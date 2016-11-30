33bc55c5099ff867a80f2b01bc5b6d264b758669
Adhe Widianjaya <adwidianjaya@gmail.com>
2016-11-30 19:42:03 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # vue-instant-loading-spinner
+
+> Instant loading spinner for use with awesome Vue
+
+## Installation
+
+``` bash
+# yarn
+yarn add vue-instant-loading-spinenr
+
+# npm
+npm install --save vue-instant-loading-spinenr
+```
+
+## Example
+
+### Markup
+
+``` html
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>vue-instant-loading-spinner</title>
+  </head>
+  <body>
+    <div id="app">
+      <vue-instant-loading-spinner ref="Spinner"></vue-instant-loading-spinner>
+      <button @click="test()">Test</button>
+    </div>
+    <!-- your script -->
+  </body>
+</html>
+```
+
+### Script
+
+``` javascript
+import Vue from 'vue'
+import VueInstantLoadingSpinner from 'vue-instant-loading-spinner'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { VueInstantLoadingSpinner },
+  methods: {
+    test: function () {
+      this.$refs.Spinner.show();
+      setTimeout(function () {
+        this.$refs.Spinner.hide();
+      }.bind(this), 2000);
+    }
+  }
+});
+```
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/VueInstantLoadingSpinner.vue
@@ -0,0 +1,145 @@
+<template>
+    <transition name="fade-spinner" v-if="showModal">
+        <div class="ini-spinner-mask">
+            <div class="ini-spinner-content text-center">
+                <component v-bind:is="spinnerStyle" :loading="loading" :color="color" :size="size" :margin="margin" :radius="radius">
+                </component>
+            </div>
+            <!-- /.ini-spinner-content -->
+        </div>
+        <!-- /.ini-spinner-mask -->
+    </transition>
+</template>
+<script>
+
+import BeatLoader from './Spinners/BeatLoader.vue'
+import BounceLoader from './Spinners/BounceLoader.vue'
+import ClipLoader from './Spinners/ClipLoader.vue'
+import DotLoader from './Spinners/DotLoader.vue'
+import FadeLoader from './Spinners/FadeLoader.vue'
+// import GridLoader from './Spinners/GridLoader.vue'
+import MoonLoader from './Spinners/MoonLoader.vue'
+// import PacmanLoader from './Spinners/PacmanLoader.vue'
+import PulseLoader from './Spinners/PulseLoader.vue'
+// import RingLoader from './Spinners/RingLoader.vue'
+import RiseLoader from './Spinners/RiseLoader.vue'
+// import RotateLoader from './Spinners/RotateLoader.vue'
+// import ScaleLoader from './Spinners/ScaleLoader.vue'
+// import SkewLoader from './Spinners/SkewLoader.vue'
+// import SquareLoader from './Spinners/SquareLoader.vue'
+import SyncLoader from './Spinners/SyncLoader.vue'
+
+const componentsList = {
+  'beat-loader': BeatLoader,
+  // 'bounce-loader': BounceLoader,
+  'clip-loader': ClipLoader,
+  'dot-loader': DotLoader,
+  'fade-loader': FadeLoader,
+  // 'grid-loader': GridLoader,
+  'moon-loader': MoonLoader,
+  // 'pacman-loader': PacmanLoader,
+  'pulse-loader': PulseLoader,
+  // 'ring-loader': RingLoader,
+  'rise-loader': RiseLoader,
+  // 'rotate-loader': RotateLoader,
+  // 'scale-loader': ScaleLoader,
+  // 'skew-loader': SkewLoader,
+  // 'square-loader': SquareLoader,
+  'sync-loader': SyncLoader
+}
+
+export default {
+  mounted () {
+    // console.log('IniLoadingSpinner ready!');
+  },
+  props: {
+    spinnerStyle: {
+      type: String,
+      default: 'rise-loader'
+      // default: 'beat-loader'
+      // default: 'dot-loader'
+      // default: 'sync-loader'
+      // default: 'pulse-loader'
+    },
+    loading: {
+      type: Boolean,
+      default: true
+    },
+    color: {
+      type: String,
+      // default: '#429bf4' // calm
+      default: '#5dc596' // green
+    },
+    size: {
+      type: String,
+      default: '25px'
+    },
+    margin: {
+      type: String,
+      default: '2px'
+    },
+    radius: {
+      type: String,
+      // default: '20px'
+      default: '100%'
+    }
+  },
+  data () {
+    var list = []
+    return {
+      notificationList: list,
+      showModal: false
+    }
+  },
+  components: componentsList,
+  methods: {
+    show: function () {
+      this.showModal = true
+    },
+    hide: function () {
+      this.showModal = false
+    }
+  }
+}
+</script>
+<style>
+.ini-spinner-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(100, 100, 100, .1);
+  display: block;
+  transition: all .3s ease;
+}
+
+.ini-spinner-content {
+  width: 200px;
+  height: 100px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  text-align: center;
+  display: block;
+  transition: all .3s ease;
+}
+
+.fade-spinner-enter {
+  opacity: 0;
+}
+
+.fade-spinner-leave-active {
+  opacity: 0;
+}
+
+.fade-spinner-enter .ini-spinner-content,
+.fade-spinner-leave-active .ini-spinner-content {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
